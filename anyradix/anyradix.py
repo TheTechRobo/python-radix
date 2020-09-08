dba92c6d8f9372ed7e256278a7e56b125777ce6c
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-09-08 11:27:36 -0400

    change not valid to invalid; use valueerror instead of Exception

--- anyradix/anyradix.py
+++ anyradix/anyradix.py
@@ -5,10 +5,10 @@ def to_base_10(number, old_base):
     figures = str(number).lower()[::-1]
     for index, elem in enumerate(figures):
         if elem not in symbol:
-            raise Exception('Not valid number')
+            raise ValueError('Invalid number')
         i = symbol.index(elem)
         if i >= old_base:
-            raise Exception('Not valid number')
+            raise ValueError('Invalid number')
         result += i * old_base**index
     return str(result)
 
@@ -17,7 +17,7 @@ def from_base_10(number, new_base):
     try:
         number = int(number)
     except:
-        raise Exception('Not valid number')
+        raise ValueError('Invalid number')
     while number > 0:
         remainder = number % new_base
         result = symbol[remainder] + result
@@ -26,7 +26,7 @@ def from_base_10(number, new_base):
 
 def cast(number, old_base, new_base):
     if old_base==1 or new_base==1:
-        raise Exception('Not valid base')
+        raise ValueError('Invalid base')
     if old_base != 10:
         number = to_base_10(number, old_base)
     if new_base != 10:
