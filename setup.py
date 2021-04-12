51c243c1c69ecf7e970a0b53fa566834f6856afa
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-04-12 11:47:38 -0400

    Remove python 2 classifiers
    For now because I am lazy we will continue support for py2
    but there will be no classifiers for it.

--- setup.py
+++ setup.py
@@ -9,19 +9,17 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='python-radix',
+setup(name='python-radix-ng',
 version='0.2',
-description='A tool for conversion numbers from one base to another',
+description='A tool for conversion numbers from one base to another, TheTechRobo edition',
 long_description=long_description,
 url='https://github.com/valbub/python-radix',
 author='valbub, yury-khrustalev, mulkieran, TheTechRobo',
 license='MIT',
 classifiers=[
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
 ],
